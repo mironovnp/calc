1aa84e6194ed8b504de1358a9b00143d6a9ef784
mironovnp <mironovnp-7@mail.ru>
2025-09-12 16:52:02 +0300

    new file:   src/main 	modified:   src/main.cpp 	modified:   src/rpn.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,41 @@
 #include <iostream>
 #include <locale>
 int main() {
-    double a,b;
-    char opr;
-    std::cout << "Введите первое число ";
-    std::cin >> a;
-    std::cout << "Введите второе число ";
-    std::cin >> b;
-    std::cout << "Введите оператор ";
-    std::cin >> opr;
-    try {
-        std::cout << operation(a,b,opr) << "\n\n";
-    } catch (const std::exception& e) {
-        std::cout << "Ошибка: " << e.what() << std::endl;
+    double history[5]={};
+    while (true) {
+        double a,b;
+        char opr, ans;
+        std::cout << "Хотите ли вы использовать историю? Ответ y или n: ";
+        std::cin >> ans;
+        if (ans=='n') {
+            std::cout << "Введите первое число: ";
+            std::cin >> a;
+            std::cout << "Введите второе число: ";
+            std::cin >> b;
+            std::cout << "Введите оператор: ";
+            std::cin >> opr;
+        } else {
+            for(int i = 0; i < 5; i++) {
+                std::cout << history[i] << ' ';
+            }
+            std::cout << "Введите первое число: ";
+            std::cin >> a;
+            std::cout << "Введите оператор: ";
+            std::cin >> opr;
+            std::cout << "Введите второе число: ";
+            std::cin >> b;
+        }
+
+
+        try {
+            std::cout << "Ответ: " << operation(a,b,opr) << "\n\n";
+            for (int i = 4; i > 0; i--) {
+                history[i] = history[i - 1];
+            }
+            history[0]=operation(a,b,opr);
+        } catch (const std::exception& e) {
+            std::cout << "Ошибка: " << e.what() << std::endl;
+        }
     }
     return 0;
 }
--- src/rpn.cpp
+++ src/rpn.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <cmath>
 double operation(double a, double b, char opr) {
     double res;
     switch (opr) {
@@ -15,6 +16,8 @@ double operation(double a, double b, char opr) {
             if (b == 0) throw std::invalid_argument("Деление на 0");
             res = a / b;
             break;
+        case '^':
+            res = pow(a,b);
         default:
             throw std::invalid_argument("Неизвестный оператор");
 
